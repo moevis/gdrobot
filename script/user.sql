a21769487c9c94535312e47bce4f402043f275d6
Moevis <moevery@gmail.com>
2016-09-28 14:17:48 +0800

    add sql

---
+++ script/user.sql
@@ -0,0 +1,49 @@
+create table user (
+	id integer primary key autoincrement,
+	name varchar(32),
+	email varchar(32),
+	phone varchar(16),
+	password char(32),
+	wx varchar(32),
+	role int default 0,
+	created timestamp default CURRENT_TIMESTAMP
+);
+
+insert into user(email, password, role) values("moevery@gmail.com", "670b14728ad9902aecba32e22fa4f6bd", 1);
+insert into user(email, password, role) values("moevery@163.com", "670b14728ad9902aecba32e22fa4f6bd", 0);
+
+create table report (
+	id integer primary key autoincrement,
+	schoolName varchar(64),
+	teamName varchar(64),
+	entry integer,
+	topic varchar(64),
+	address varchar(255),
+	userId integer,
+	teachers varchar(255) default "[]",
+	students varchar(255) default "[]",
+	status integer default 0,
+	created timestamp default CURRENT_TIMESTAMP
+);
+
+create table notice (
+	id integer primary key autoincrement,
+	userId integer,
+	content varchar(500),
+	status integer default 0,
+	created timestamp default CURRENT_TIMESTAMP
+);
+
+create table article (
+	id integer primary key autoincrement,
+	content text,
+	created timestamp default CURRENT_TIMESTAMP
+);
+
+create table option (
+	key varchar(32) primary key,
+	value varchar(128) 
+);
+
+insert into option(key, value) values('START_DATE', '2022-12-12 22:22:22');
+insert into option(key, value) values('END_DATE', '2023-12-12 22:22:22');
